5f7d1b43e11e4b4e48b3c5a9549c4c1fb3635594
Mateus <mssantos8mm@gmail.com>
2024-06-01 00:00:53 -0300

    feat: Adicionado matriz de confusão

--- src/network/utils.py
+++ src/network/utils.py
@@ -20,7 +20,7 @@ def separa_dados_por_letras(data: List[List[int]], labels: List[List[int]]) -> d
     """Retorna um dicionário com letras como chave e os dados correspondentes como valor"""
     result = {}
     for i in range(len(labels)):
-        key = Loader.converter_binario_rotulo(labels[i])
+        key = Loader.converter_binario_para_letra(labels[i])
         if key not in result:
             result[key] = []
         result[key].append(data[i])
@@ -54,3 +54,24 @@ async def distribui_valores(data: List[List[int]], labels: List[List[int]], perc
         test_labels.extend(letra_em_binario * test_count)
 
     return train_array, train_labels, validation_array, validation_labels, test_array, test_labels
+
+
+def imprimir_matriz_confusao(lista_true: list[str], lista_output: list[str]):
+    # Inicializar a matriz de confusão 26x26
+    confusion_matrix = [[0 for _ in range(26)] for _ in range(26)]
+
+    # Criar um dicionário para mapear letras minúsculas para índices
+    letra_para_indice = {chr(i): i - 97 for i in range(97, 123)}  # a-z -> 0-25
+
+    # Preencher a matriz de confusão
+    for true, pred in zip(lista_true, lista_output):
+        true_index = letra_para_indice[true]
+        pred_index = letra_para_indice[pred]
+        confusion_matrix[true_index][pred_index] += 1
+
+    # Imprimir a matriz de confusão
+    print("Matriz de Confusão:")
+    print("     " + " ".join([chr(i) for i in range(97, 123)]))  # Header
+    for i in range(26):
+        row = " ".join([str(confusion_matrix[i][j]) for j in range(26)])
+        print(f"{chr(i + 97)}    {row}")
